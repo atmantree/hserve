fd554ba48aa3d7f60c6eaefddc8b89c25691acf6
Carlos Gustavo Ruiz <atmantree@gmail.com>
2018-04-13 20:14:39 -0500

    read current directory

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      readCurrentDir,
     ) where
+
+import System.Directory
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+readCurrentDir :: IO ()
+readCurrentDir = do
+    currentPath <- getCurrentDirectory
+    files <- getDirectoryContents currentPath
+    print files
+
